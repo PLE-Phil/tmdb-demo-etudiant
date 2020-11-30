f2cabf8208f2ad37977d202acb9c415cbe3e5709
Philippe <1640662@cegepmontpetit.ca>
2020-11-30 14:54:08 -0500

    Fin du 2e cours pour le TP3.

--- src/js/script.js
+++ src/js/script.js
@@ -19,7 +19,9 @@ class MovieDB{
 
         this.baseURL = "https://api.themoviedb.org/3";
 
-        this.imgPath = "https://image.tmdb.org/t/b/";
+        this.imgPath = "https://image.tmdb.org/t/p/";
+
+        // this.largeurAffiche = ["w92", "w154", "w185", "w342", "w500", "w780"];
 
         this.totalFilm = 8;
 
@@ -54,9 +56,28 @@ class MovieDB{
 
     afficheDernierFilm(data){
 
-        for (let i = 0; i < data.length; i++) {
+        for (let i = 0; i < this.totalFilm; i++) {
             console.log(data[i].title);
-            console.log(data[i].overview);
+            // console.log(data[i].overview);
+
+            let unArticle = document.querySelector(".template>article.film").cloneNode(true); // Cloner tout ce qu'il y a à l'intérieur de la classe film dans le template (voir index.html).
+
+            unArticle.querySelector("h2").innerHTML = data[i].title; // On va chercher les titres des films dans le tableau et on l'associe aux titres des clones.
+
+            unArticle.querySelector("p.description").innerHTML = data[i].overview || "Pas de description disponible"; // On va chercher les descriptions des films (qui ont le nom overview) dans le tableau et on l'associe aux titres des clones. OU Ça la remplace par le texte : Pas de description.
+
+            let src = this.imgPath + "w185" + data[i].poster_path; // Créer une variable source de l'image reconstruisant le chemin vers le bon dossier des images. ".poster-path" correspond aux images dans le tableau des films.
+            console.log(src);
+
+            let uneImage = unArticle.querySelector("img");
+            uneImage.setAttribute("src", src);
+            uneImage.setAttribute("alt", data[i].title);
+
+
+            document.querySelector(".liste-films").appendChild(unArticle); // On apporte le clone dans une autre partie du html. Ici, dans la classe liste-films
+
+
+
         }
 
     }
